568da47f257798c9c443f0906b734bba557b1bf0
rockclimber112358 <rockclimber112358@gmail.com>
2015-08-28 09:17:40 +0200

    Modified data loading script

--- createDatasets.R
+++ createDatasets.R
@@ -38,3 +38,33 @@ finalData = rbindlist(d, fill = TRUE)
 Sys.time() - start
 nrow(finalData)
 save(d, file = paste0("~/Documents/Github/romeHousePrices/Data/detailData_", time, ".RData"))
+
+## Big sample, Immobiliare Vendita
+listingPages = getPropertyUrlsImmobiliare(numPages = 100000)
+save(listingPages, file = paste0("~/Documents/Github/romeHousePrices/Data/listingPagesImbVend_", time, ".RData"))
+start = Sys.time()
+d = lapply(listingPages, getPropertyDetailsImmobiliare)
+finalData = rbindlist(d, fill = TRUE)
+Sys.time() - start
+nrow(finalData)
+save(d, file = paste0(workingDir, "/Data/detailImbVend_", time, ".RData"))
+
+## Big sample, Mio Affitto
+listingPages = getPropertyUrlsMioAffitto(numPages = 100000)
+save(listingPages, file = paste0("~/Documents/Github/romeHousePrices/Data/listingPages_", time, ".RData"))
+start = Sys.time()
+d = lapply(listingPages, getPropertyDetails)
+finalData = rbindlist(d, fill = TRUE)
+Sys.time() - start
+nrow(finalData)
+save(d, file = paste0("~/Documents/Github/romeHousePrices/Data/detailData_", time, ".RData"))
+
+## Small sample, Immobiliare Affitto
+listingPages = getPropertyUrlsImmobiliare(numPages = 100000, type = "affitto")
+save(listingPages, file = paste0("~/Documents/Github/romeHousePrices/Data/listingPagesImbAff_", time, ".RData"))
+start = Sys.time()
+d = lapply(listingPages, getPropertyDetailsImmobiliare)
+finalData = rbindlist(d, fill = TRUE)
+Sys.time() - start
+nrow(finalData)
+save(d, file = paste0(workingDir, "/Data/detailImbAff_", time, ".RData"))
