42a774648b28a363928931e071b9801046850060
Michael Rahija <michaelrahija@Michaels-MBP-2.lan>
2015-09-21 21:24:33 +0200

    Added code to parse table

--- R/getPropertyDetailsCasa.R
+++ R/getPropertyDetailsCasa.R
@@ -18,28 +18,6 @@ getPropertyDetailsCasa = function(url){
         warning("Page could not be read!  Returning NULL.")
         return(NULL)
     }
-    #propertyDetails = html_nodes(htmlCode, "#details td")                      come back!
-    #propertyDetails = gsub("(\t|\n|:|\\s$)", "", html_text(propertyDetails))
-    
-    #extract main table
-    mainDetails = html_nodes(htmlCode,"li , #features span")
-    mainDetails = html_text(mainDetails)
-    
-    #price
-    prezzo = html_nodes(htmlCode, ".price")
-    prezzo = html_text(prezzo)
-    
-    #superficie
-    superficie = grepl("^[00-99]+ mq",mainDetails)
-    superficie = mainDetails[superficie]
-    
-    #bagno
-    bagni = grepl("Bagni:", mainDetails)
-    bagni = mainDetails[bagni]
-    
-    #locali
-    locali = grepl("Locali:", mainDetails)
-    locali = mainDetails[locali]
     
     
     #indirizzo
@@ -72,63 +50,28 @@ getPropertyDetailsCasa = function(url){
     riferimento = html_text(riferimento)
     riferimento = gsub("Codice annuncio ","", riferimento)
     
-    #tipologia
-    tipologia = html_nodes(htmlCode,"ul:nth-child(1) li:nth-child(3)")
-    tipologia = html_text(tipologia)
-    test <- grepl("Tipologia:", tipologia)
-    tipologia <- tipologia[test]
-    tipologia <- gsub("Tipologia:","",tipologia)
-   
+    #############################
+    #extract features from table#
+    #############################
+    table.features <- html_nodes(htmlCode,"#features li")
+    table.features <- html_text(table.features)
     
-    data = data.table(prezzo = prezzo, 
-                      superficie = superficie,
-                      bagni = bagni,
-                      locali = locali,
-                      indirizzo = indirizzo,
-                      zona = zona,
-                      descrizione = descrizione,
-                      condominio = condominio,
-                      riferimento = riferimento,
-                      tipologia = tipologia)
+    #split to create names from values, names are features
+    propertyDetails = strsplit(table.features, ":")
+    names = lapply(propertyDetails, function(x) x[1])
+    values = lapply(propertyDetails, function(x) x[2])
     
+    #remove column names
+    col.names <- c("Caratteristiche generali","Caratteristiche interne","Caratteristiche esterne")
+    test <- match(col.names,names)
+    names <- names[-test]
+    values <- values[-test]
     
+    #if a name is present without a value, then value = TRUE, example: ascensore
+    replace <- is.na(values)
+    values[replace] <- "TRUE"
+    
+    #combined names, values into df and merge w/ features not in table
   
-    
-    data
-    mainDetails2 = html_nodes(htmlCode, ".featureList")
-    mainDetails2 = html_text(mainDetails2)
-    mainDetails = gsub("(\n|\t)", "", html_text(mainDetails))
-#     mainDetails = strsplit(mainDetails, split = "\\|")[[1]]
-#     mapDetails = html_nodes(htmlCode, ".indirizzo_mappa")
-#     mapDetails = lapply(mapDetails, html_text)
-#     pictureCount = html_nodes(htmlCode, "#dettaglio_cont_bigimg")
-#     if(length(pictureCount) == 0){
-#         pictureCount = 0
-#     } else {
-#         pictureCount = gsub("(\n|\t|.* di| )", "",
-#                             html_text(pictureCount[[1]]))
-#     }
-#     description = html_text(html_nodes(htmlCode, ".descrizione")[[1]])
-#     data = data.table(
-#         ## Remove all non-numeric characters
-          superficie = gsub("\\ mq", " m²", mainDetails[17])
-#         locali = gsub("[^(0-9)]", "", mainDetails[2]),
-          bagni = gsub("Bagni:", "", mainDetails[6])
-#         prezzio = gsub("[^(0-9)]", "", mainDetails[4]),
-#         indirizzio = gsub("(\n|\t)", "", mapDetails[1]),
-#         zona = gsub("(\n|\t)", "", mapDetails[2]),
-#         quartiere = gsub("(\n|\t)", "", mapDetails[3]),
-#         description = gsub("(\n|\t)", "", description),
-#         pictureCount = pictureCount
-#     )
-#     names = propertyDetails[seq(1, length(propertyDetails)-1, 2)]
-#     names = gsub(" ", "\\.", names)
-#     values = propertyDetails[seq(2, length(propertyDetails), 2)]
-#     badNames = names == ""
-#     names = names[!badNames]
-#     values = values[!badNames]
-#     for(i in 1:length(names)){
-#         data[, (names[i]) := values[i]]
-#     }
-#     return(data)
+
 }
